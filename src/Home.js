a5d8c74bfdef77f609f6e3c63a7f1cb66f0b30ee
erikdodge <edodge-chwy>
2020-08-05 22:29:33 -0500

    UI ONLY SIGN UP

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
+import SignUp from "./SignUp";
 
 export default function Home(){
     return(
         <Box display={'flex'}>
-            <Button primary title={`Enter Meal`}>Enter Meal</Button>
+            <SignUp/>
         </Box>
     )
 }
